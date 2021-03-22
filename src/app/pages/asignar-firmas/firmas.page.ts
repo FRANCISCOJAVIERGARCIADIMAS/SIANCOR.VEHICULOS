c1fd533913bbd81d22cf12bbc72d28a3da22c4ea
Javier Garcia <javiergarciadimas@gmail.com>
2021-03-21 20:49:28 -0700

    detalle menor

--- src/app/pages/asignar-firmas/firmas.page.ts
+++ src/app/pages/asignar-firmas/firmas.page.ts
@@ -8,13 +8,12 @@ import { Component, Input, OnInit } from '@angular/core';
 export class FirmasPage implements OnInit {
 
   @Input() signatureEmitted: string;
+  
   constructor() { }
 
   ngOnInit() {
     
   }
-  ver(){
-    console.log(this.signatureEmitted);
-  }
+  
 
 }
